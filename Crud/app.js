1a9b01be559c52b0ab9d2dc49babe6f1a609a65e
Kowsar-Dev <kowsara276@gmail.com>
2025-10-20 17:02:13 +0600

    first commit

---
+++ Crud/app.js
@@ -0,0 +1,22 @@
+// const URL = "https://uselessfacts.jsph.pl/random.json?language=en";
+// let fact = document.querySelector("#fact");
+// let btn = document.querySelector ("#btn")
+
+
+// const getFacts = async () =>{
+//   let respose = await fetch (URL);
+//   let data = await respose.json();
+//   fact.innerText = data.text
+// }
+
+// // function getFacts (){
+// //   fetch(URL).then((res)=>{
+// //     return res.json();
+// //   }).then((data)=>{
+// //     fact.innerText = data.text
+
+// //   })
+// // }
+
+// btn.addEventListener("click",getFacts);
+
